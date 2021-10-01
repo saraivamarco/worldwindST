cdee9b1eef05919e42a4eacb34836c78cd8381ac
Marco Saraiva da Silva <marcoasilvapt@gmail.com>
2021-10-01 01:12:55 +0100

    Added worldwind js features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './400x230-splash-nww.png';
 import './App.css';
 import * as satellite from "satellite.js";
-import {getAllItems} from './Client';
 import {useState, useEffect, useContext} from 'react';
-import Button from 'react-bootstrap/Button';
-import { BrowserRouter as Router, Switch, Route, Redirect, Link } from "react-router-dom";
 import AuthApi from "./AuthApi";
-//import Cookies from 'js-cookie';
 import axios from 'axios';
+import * as WorldWind from "@nasaworldwind/worldwind";
 
-function invokeSatellite() {
+/**
+ * TLE_LINE0: "0 YUNHAI 1-02 DEB"
+ * @param {*} tle1  TLE_LINE1: "1 49267U 19063AT  21271.01286517  .00004529  00000-0  16332-2 0  9997"
+ * @param {*} tle2  TLE_LINE2: "2 49267  98.5535 296.3547 0004348 107.3701 252.8041 14.34111012 27008"
+ */
+
+function invokeSatellite(tleLine1, tleLine2) {
   // Sample TLE
-  var tleLine1 = '1 25544U 98067A   19156.50900463  .00003075  00000-0  59442-4 0  9992';
-  var tleLine2 = '2 25544  51.6433  59.2583 0008217  16.4489 347.6017 15.51174618173442';    
+  //var tleLine1 = '1 25544U 98067A   19156.50900463  .00003075  00000-0  59442-4 0  9992';
+  //var tleLine2 = '2 25544  51.6433  59.2583 0008217  16.4489 347.6017 15.51174618173442';    
 
   var satrec = satellite.twoline2satrec(tleLine1, tleLine2);
   var positionAndVelocity = satellite.propagate(satrec, new Date());
 
-  console.log(positionAndVelocity);
+  //console.log(positionAndVelocity);
+  return positionAndVelocity;
 }
 
 function App() {
@@ -27,69 +30,37 @@ function App() {
 
   useEffect(() => {
     console.log("component is mounted");
-    //readCookie();
-    fetchData();
   }, []);
 
-  /** consuming a dummy rest service - just for testing purposes */
-  const fetchData = () =>
-        getAllItems()
-            .then(res => res.json())
-            .then(data => {
-                //console.log(data.data);
-                setData(data.data);
-        }).catch(err => {
-                console.log(err.response)
-                err.response.json().then(res => {
-                    console.log(res);
-                });
-        }).finally(() => {
-          setFetching(false);
-        });
-
-  function renderData() {
-    const list = data.map(obj => <dt key={obj.id}>{obj.name}</dt>)
-    return (
-      <dl>{list}</dl>
-    )
-  }
-  /** end of consuming a dummy rest service */
-
-  /** It was planned to be used for getting the cookie from Space-Track but the feature is buggy
-  const readCookie = () => {
-    const user = Cookies.get("spacetrack_csrf_cookie");
-    if(user){
-      setAuth(true);
-    }
-  }
-   */
- 
   return (
     <div className="App">
-
-      <AuthApi.Provider value={{auth,setAuth}}>
-        <Router>
-          <Routes></Routes>
-        </Router>
-      </AuthApi.Provider>
-      
+      <Home/>
     </div>
   );
 }
 
 //https://www.space-track.org/documentation#howto-api_intro
 const Home = () => {
-  const Auth = useContext(AuthApi);
 
-  const data = 
-  {
-    "identity": "<user_name>",
-    "password": "<password>",
-    "query": "https://www.space-track.org/basicspacedata/query/class/gp/OBJECT_TYPE/debris/limit/3"
+   // Create a WorldWindow for the canvas.
+   var wwd = new WorldWind.WorldWindow("canvasOne");
+  
+   // Add some image layers to the WorldWindow's globe.
+   wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+   wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+   // Add a compass, a coordinates display and some view controls to the WorldWindow.
+   wwd.addLayer(new WorldWind.CompassLayer());
+   wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+   wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+
+  const data = {
+    "identity": "marcoasilvapt@gmail.com",
+    "password": "Spacenuts2021!*",
+    "query": "https://www.space-track.org/basicspacedata/query/class/gp/OBJECT_TYPE/debris"
   };
 
-  const extra = 
-  {
+  const extra = {
     withCredentials: true,
     mode: 'no-cors',
     credentials: 'same-origin',
@@ -102,95 +73,52 @@ const Home = () => {
     }
   }
 
-
-  const handleOnClick = () => {
-    invokeSatellite();
-    Auth.setAuth(true);
-    //Cookies.set("spacetrack_csrf_cookie","578142329a6bcf8024271040928aeb0c");
-
-    //let cookie = Cookies.get("chocolatechip");
-    //if(cookie === undefined){
+  const fetchSpaceTrackData = () => {
+    //Auth.setAuth(true);
     axios.post("https://www.space-track.org/ajaxauth/login", data, extra).then(response => {
       if(response.status === 200){
-        //debugger;
-        console.log(response);
-        //Cookies.set("chocolatechip", response.data);
+        
+        window.mydata = response.data;
+
+        for (var i = 0; i < response.data.length; i++) {
+          console.log();
+          var coord = invokeSatellite(response.data[i].TLE_LINE1,response.data[i].TLE_LINE2);
+
+
+          //debugger;
+          // Add a placemark
+          var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
+          wwd.addLayer(placemarkLayer);
+
+          var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
+    
+          placemarkAttributes.imageOffset = new WorldWind.Offset(
+              WorldWind.OFFSET_FRACTION, 0.3,
+              WorldWind.OFFSET_FRACTION, 0.3);
+    
+          placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
+          placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
+              WorldWind.OFFSET_FRACTION, 0.5,
+              WorldWind.OFFSET_FRACTION, 1.0);
+    
+    
+          //placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/pushpins/plain-red.png";
+          var position = new WorldWind.Position(coord.position.x, coord.position.y, coord.position.z);
+          var placemark = new WorldWind.Placemark(position, false, placemarkAttributes);
+          placemark.label = response.data[i].TLE_LINE0;
+    
+          // debugger;
+          placemarkLayer.addRenderable(placemark);
+        } 
       }
     }).catch(err => console.warn(err));
-    //}
   }
   return (
     <div>
-      <header className="App-header">
-        <h1>Welcome</h1>
-        <a href="https://worldwind.arc.nasa.gov">
-        <img src={logo} className="App-logo" alt="logo" /></a>
-        <p></p>
-        <Button onClick={handleOnClick}>Query Space-Track.org for Debris</Button>
+      <header className="App-header"> 
+      {fetchSpaceTrackData()}
       </header>
     </div>
   )
 }
-
-const Dashboard = () => {
-  const Auth = useContext(AuthApi);
-  const handleOnClick = () => {
-    Auth.setAuth(false);
-    //Cookies.remove("spacetrack_csrf_cookie");
-  }
-  return (
-    <div>
-        <h1>Dashboard</h1>
-        <Button onClick={handleOnClick}>Go Back</Button>
-        <div id="table-wrapper">
-          <div id="table-scroll">
-            <table>
-                <tbody>
-                  <tr>
-                    <td>
-                    {/*renderData()*/}
-                    </td>
-                  </tr>
-                </tbody>
-            </table>
-          </div>
-        </div>
-    </div>
-  )
-}
-
-const Routes = () => {
-  const Auth = useContext(AuthApi);
-  return (
-    <Switch>
-      <ProtectedLogin path="/home" auth={Auth.auth} component={Home}/>
-      <ProtectedRoute path="/dashboard" auth={Auth.auth} component={Dashboard}/>
-    </Switch>
-  )
-}
-
-const ProtectedRoute = ({auth, component:Component, ...rest}) => {
-  return (
-    <Route
-      {...rest}
-      render = {()=> auth? 
-        (<Component/>):
-        (<Redirect to="/home"/>)
-      }
-    />
-  )
-}
-
-const ProtectedLogin = ({auth, component:Component, ...rest}) => {
-  return (
-    <Route
-      {...rest}
-      render = {()=> !auth? 
-        (<Component/>):
-        (<Redirect to="/dashboard"/>)
-      }
-    />
-  )
-}
-
 export default App;
